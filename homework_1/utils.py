8c55c6a65398d039160f358cf0daea1af1382225
Evie <evita@stenqvist.co>
2021-11-08 13:15:49 +0100

    Basic test decorator printing func name and docstring

--- homework_1/utils.py
+++ homework_1/utils.py
@@ -1,2 +1,21 @@
-# Seems to be pointless to generate primes when it costs so little to just store 100 primes.
-primes = [2,3,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547]
+class terminal:
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
+
+
+def test(func):
+    def func_with_name_and_docstring(*args, **kwargs):
+        print("\n")
+        print(f"{terminal.BOLD} => {func.__name__} {terminal.END}")
+        print(f"{terminal.DARKCYAN}{func.__doc__}{terminal.END}")
+        func(*args, **kwargs)
+
+    return func_with_name_and_docstring
