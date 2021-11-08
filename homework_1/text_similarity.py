4da54bc0736948afb783ff6d8c68921aae103f88
Evie <evita@stenqvist.co>
2021-11-08 14:27:19 +0100

    Refactored shingling again
    I'm sorry, it's trivial, but it was all back to front and refactoring soothes me.

--- homework_1/text_similarity.py
+++ homework_1/text_similarity.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from typing import Callable
 from utils import primes, test
 import math
@@ -19,71 +20,70 @@ class TestDocuments:
     I = "The cat plots giving the dog a chocolate delight"
     J = "What a fright, this spite, at midnight"
 
+    @staticmethod
+    def generator():
+        letters = string.ascii_uppercase[:10]
+        for letter in letters:
+            yield letter, getattr(TestDocuments, letter)
 
+@dataclass
 class Shingling:
-    ### Instance attributes
     k: int
-    document: str
-    document_name: str
-    hashed: set
+    all_hashed_shingles: set[int] = field(default_factory=set)
+    doc2hashed: dict = field(default_factory=dict)
+    path: str = "shingling.pickle"
 
-    ### Class attributes
-    all_hashed_shingles: set[int] = set()
-    doc2hashed: dict = {}
-    path = "shingling.pickle"
-
-    def __init__(self, k, document, document_name):
-        self.k = k
-        self.document = document
-        self.document_name = document_name
-        self.hashed = set()
-
-    def prepare_document(self):
+    @staticmethod
+    def prepare_document(document):
         # The simplest of replacements, newlines and . become a space
-        self.document = self.document.replace("\n", " ").replace(".", " ")
+        return document.replace("\n", " ").replace(".", " ")
 
-    def populate_hashed(self):
-        self.prepare_document()
+    def add_document(self, document: str, document_name: str) -> list[int]:
+        document = self.prepare_document(document)
 
-        number_of_shingles = len(self.document) + 1 - self.k
+        number_of_shingles = len(document) + 1 - self.k
 
+        hashed_document = set()
         for index in range(number_of_shingles):
-            shingle = self.document[index : index + self.k]
+            shingle = document[index : index + self.k]
             hashed_shingle = hash(shingle)
-            Shingling.all_hashed_shingles.add(hashed_shingle)
-            self.hashed.add(hashed_shingle)
-        Shingling.doc2hashed[self.document_name] = self.hashed
-        return self.hashed
+            self.all_hashed_shingles.add(hashed_shingle)
+            hashed_document.add(hashed_shingle)
+        self.doc2hashed[document_name] = hashed_document
+        return hashed_document
 
-    @classmethod
-    def get_all_hashed_shingles(cls):
-        assert len(cls.all_hashed_shingles) != 0, "all_hashed_shingles shouldn't be 0!"
+    def add_documents(self, documents: dict[str, str]):
+        for document_name, document in documents.items():
+            self.add_document(document, document_name)
 
-        return cls.all_hashed_shingles
+    def get_all_hashed_shingles(self) -> list[int]:
+        assert len(self.all_hashed_shingles) != 0, "all_hashed_shingles shouldn't be 0!"
 
-    @classmethod
-    def get_doc2hashed(cls):
-        assert len(cls.doc2hashed.keys()) != 0, "doc2hashed shouldn't be 0!"
-        return cls.doc2hashed
+        return self.all_hashed_shingles
 
-    @classmethod
-    def save_to_file(cls):
-        with open(cls.path, "wb") as f:
+    def get_doc2hashed(self) -> dict[str, str]:
+        assert len(self.doc2hashed.keys()) != 0, "doc2hashed shouldn't be 0!"
+        return self.doc2hashed
+
+    def save_to_file(self):
+        with open(self.path, "wb") as f:
             data_dict = {
-                "all_hashed_shingles": cls.all_hashed_shingles,
-                "doc2hashed": cls.doc2hashed,
+                "k": self.k,
+                "all_hashed_shingles": self.all_hashed_shingles,
+                "doc2hashed": self.doc2hashed,
             }
             pickle.dump(data_dict, f)
 
     @classmethod
-    def load_from_file(cls):
-        if not os.path.isfile(cls.path):
+    def load_from_file(cls, path) -> "Shingling":
+        if not os.path.isfile(path):
             raise Exception("Previous runs have not been saved to file.")
-        with open(cls.path, "rb") as f:
+        with open(path, "rb") as f:
             data_dict = pickle.load(f)
-            cls.all_hashed_shingles = data_dict["all_hashed_shingles"]
-            cls.doc2hashed = data_dict["doc2hashed"]
-            return cls.all_hashed_shingles, cls.doc2hashed
+            shingling = Shingling(k=data_dict["k"])
+            shingling.all_hashed_shingles = data_dict["all_hashed_shingles"]
+            shingling.doc2hashed = data_dict["doc2hashed"]
+            return shingling
 
 
 class CompareSets:
@@ -119,7 +119,9 @@ class MinHashing:
             func = self.linear_hash(a, b, prime)
             self.hash_functions.append(func)
 
-    def build_minhash_signatures(self, items: set, doc2items: dict[str, set]):
+    def build_minhash_signatures(
+        self, items: set, doc2items: dict[str, set]
+    ) -> dict[str, list[int]]:
         minhashed = {
             doc_identifier: [math.inf] * self.n_functions
             for doc_identifier in doc2items.keys()
@@ -139,12 +141,6 @@ class MinHashing:
 
 class CompareSignatures:
     def approximate_jaccard_similarity(A: list, B: list):
-        # Maybe using counters and comparing the differences would be
-        # quick?
-        # counters = {}
-        # for key, value in signatures.items():
-        #     counters[key] = Counter(value)
-        # pprint(counters)
         assert len(A) == len(B), "Length of signatures should be equal"
         # Naive way
         equal_count = 0
@@ -158,9 +154,11 @@ class CompareSignatures:
 
 @test
 def test_simple():
-    """Trivial test jaccard test with two very similar sentences"""
-    A = Shingling(3, TestDocuments.A, "A").populate_hashed()
-    B = Shingling(3, TestDocuments.B, "B").populate_hashed()
+    """Trivial jaccard test with two very similar sentences"""
+    shingling = Shingling(k=3)
+    shingling.add_documents(dict(A=TestDocuments.A, B=TestDocuments.B))
+    A = shingling.doc2hashed["A"]
+    B = shingling.doc2hashed["B"]
 
     similarity = CompareSets.jaccard_similarity(A, B)
     print(f"Similarity between A and B {similarity}")
@@ -171,16 +169,19 @@ def test_simple():
 def test_fradulent_email_jaccard():
     """Get 5 very similar fradulent emails and 5 very disimilar emails"""
     directory = "./emails"
+    pickle_path = "fradulent_emails_shingling.pickle"
+    if os.path.isfile("fradulent_emails_shingling.pickle"):
+        shingling = Shingling.load_from_file(pickle_path)
+    else:
+        shingling = Shingling(k=7, path=pickle_path)
+        for filename in os.listdir(directory):
+            content = ""
+            with open(f"{directory}/{filename}", "r") as f:
+                content = f.read()
+            shingling.add_document(document=content, document_name=filename)
+        shingling.save_to_file()
 
-    for filename in os.listdir(directory):
-        content = ""
-        with open(f"{directory}/{filename}", "r") as f:
-            content = f.read()
-        shingler = Shingling(k=7, document=content, document_name=filename)
-        shingler.populate_hashed()
-
-    Shingling.save_to_file()
-    doc2hashed = Shingling.doc2hashed
+    doc2hashed = shingling.doc2hashed
 
     similar_n = 5
     disimilar_n = 5
@@ -209,15 +210,12 @@ def test_fradulent_email_jaccard():
 @test
 def test_simple_build_minhash_signatures():
     """Build signatures for our simple sentences"""
-    letters = string.ascii_uppercase[:10]
-    for letter in letters:
-        document = getattr(TestDocuments, letter)
-        Shingling(k=3, document=document, document_name=letter).populate_hashed()
-    Shingling.path = "test_documents.pickle"
-    Shingling.save_to_file()
+    shingling = Shingling(k=3)
+    for letter, document in TestDocuments.generator():
+        shingling.add_document(document=document, document_name=letter)
 
     n_functions = 20
-    all_hashed, doc2hashed = Shingling.all_hashed_shingles, Shingling.doc2hashed
+    all_hashed, doc2hashed = shingling.all_hashed_shingles, shingling.doc2hashed
     signatures = MinHashing(n_functions=n_functions).build_minhash_signatures(
         all_hashed, doc2hashed
     )
@@ -232,24 +230,28 @@ def test_compare_jaccard_and_minhash_signatures():
     """Compare two very similar sentences aswell as two disimilar
     for the jaccard similarity and the minhash approximation.
     """
-    A = Shingling(3, TestDocuments.A, "A").populate_hashed()
-    B = Shingling(3, TestDocuments.B, "B").populate_hashed()
-    H = Shingling(3, TestDocuments.H, "H").populate_hashed()
-
+    shingling = Shingling(k=5)
+    shingling.add_documents(
+        {"A": TestDocuments.A, "B": TestDocuments.B, "H": TestDocuments.H}
+    )
     print(f"A: {TestDocuments.A}")
     print(f"B: {TestDocuments.B}")
     print(f"H: {TestDocuments.H} \n")
 
-    all_hashed = Shingling.get_all_hashed_shingles()
-    doc2hashed = Shingling.get_doc2hashed()
+    all_hashed = shingling.get_all_hashed_shingles()
+    doc2hashed = shingling.get_doc2hashed()
 
-    n_functions = 100
+    n_functions = 200
     signatures = MinHashing(n_functions=n_functions).build_minhash_signatures(
         all_hashed, doc2hashed
     )
 
-    jaccard_similarity_similar = CompareSets.jaccard_similarity(A, B)
-    jaccard_similarity_disimilar = CompareSets.jaccard_similarity(A, H)
+    jaccard_similarity_similar = CompareSets.jaccard_similarity(
+        doc2hashed["A"], doc2hashed["B"]
+    )
+    jaccard_similarity_disimilar = CompareSets.jaccard_similarity(
+        doc2hashed["A"], doc2hashed["H"]
+    )
 
     approx_similarity_similar = CompareSignatures.approximate_jaccard_similarity(
         signatures["A"], signatures["B"]
@@ -263,23 +265,14 @@ def test_compare_jaccard_and_minhash_signatures():
     )
     # TODO: The disimilar approx is still well over 0 when using less than 150 hash functions
     # is this normal?! Is there something off with our hash functions?
-    print(
-        f"Disimilar Jaccard similarity {jaccard_similarity_disimilar}"
-    )
+    print(f"Disimilar Jaccard similarity {jaccard_similarity_disimilar}")
     print(
         f"Disimilar Approximate similarity {approx_similarity_disimilar} with {n_functions} hash functions"
     )
 
 
-def __clear_shingling():
-    Shingling.all_hashed_shingles = set()
-    Shingling.doc2hashed = {}
-
-
 if __name__ == "__main__":
-    #test_fradulent_email_jaccard()
+    # test_fradulent_email_jaccard()
     test_simple()
-    __clear_shingling()
     test_simple_build_minhash_signatures()
-    __clear_shingling()
     test_compare_jaccard_and_minhash_signatures()
