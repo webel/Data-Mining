28f16fdfc6202a88cbbdd3551608faf2d3862d8f
Evie <evita@stenqvist.co>
2021-11-18 19:16:09 +0100

    Updated a-priori :tada:

---
+++ homework_2/README.md
@@ -0,0 +1,14 @@
+# Homework 2: Discovery of Frequent Itemsets and Association Rules
+
+## Dataset
+
+### Simple dataset
+
+File `simple_test_abc.dat`. Used the dataset from slide 43 in lecture 3:
+
+```sh
+A C D
+B C E
+A B C E
+B E
+```
--- homework_2/a-priori.py
+++ homework_2/a-priori.py
@@ -1,20 +1,20 @@
+from collections import defaultdict
 from itertools import combinations
 
+dataset_path = "simple_test_abc.dat"
+
 def first_count():
-    all_items = {}
-    with open("T10I4D100K.dat", "r") as f:
+    # Key start with 0 count
+    all_items = defaultdict(lambda: 0)
+    with open(dataset_path, "r") as f:
         while True:
             line = f.readline()
             if not line:
                 break
             items = line.split()
             for i in items:
-                if i in all_items:
-                    all_items[i] += 1
-                else:
-                    all_items[i] = 1
+                all_items[i] += 1
     f.close()
-    print(all_items)
     return all_items
 
 
@@ -34,42 +34,37 @@ def generate_candidate_itemsets(items, k):
 def prune_non_frequent(itemset, s):
     to_delete = []
     for i in itemset:
-        if itemset[i] < s:
+        if itemset[i] <= s:
             to_delete.append(i)
     for j in to_delete:
         del itemset[j]
-    print("to delete: ")
+    print("\n To delete: ")
     print(to_delete)
     return itemset
 
 
 def count_support_and_prune(C, k, s):
-    itemsets = {}
-    with open("T10I4D100K.dat", "r") as f:
+    itemsets = defaultdict(lambda: 0)
+    with open(dataset_path, "r") as f:
         counter = 0
         while True:
             line = f.readline()
             if not line:
                 break
-            items = line.split()
-
-            bucket_combinations = generate_candidate_itemsets(set(items), k)
+            items = sorted(set(line.split()))
+            bucket_combinations = generate_candidate_itemsets(items, k)
             for comb in bucket_combinations:
-                #print(comb)
                 if comb in C:
                     # TODO: varför blir det fel här?
                     #  Den hittar inte alla plus att det blir typ olika varje gång man kör koden?
                     print("this combination was in C! ")
                     print(comb)
-                    if comb in itemsets:
-                        itemsets[comb] += 1
-                    else:
-                        itemsets[comb] = 1
+                    itemsets[comb] += 1
 
             counter +=1
+    print(f"\n Number of processed lines: {counter}")
 
     f.close()
-    print("counter: " + str(counter))
     # Prune the itemset
     print("found this: ")
     print(itemsets)
@@ -82,10 +77,15 @@ def count_support_and_prune(C, k, s):
 def main():
     all_items = first_count()
     # find support threshold
-    s = round(len(all_items)*0.01)
-    print(s)
+    # s = round(len(all_items)*0.01)
+    # NOTE: we set it to 1 for our very simple test! Change back for real data
+    s = 1
+    print(f"Support threshhold is: {s}")
     L1 = prune_non_frequent(all_items, s)
+    L1 = sorted(set(L1))
     C2 = generate_candidate_itemsets(L1, 2)
+    C2 = list(C2)
+    assert list(C2) == [('A', 'B'), ('A', 'C'), ('A', 'E'), ('B', 'C'), ('B', 'E'), ('C', 'E')]
     # TODO: blir snyggare sen, men bara för test nu för olika k 
     k = 2
     L2 = count_support_and_prune(C2, k, s)
