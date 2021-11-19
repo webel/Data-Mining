fd973546873a50a1c7ed7a1eeb8c0483a551204f
Evie <evita@stenqvist.co>
2021-11-19 12:08:41 +0100

    💨 Faster

--- homework_2/a-priori-v2.py
+++ homework_2/a-priori-v2.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from itertools import chain, combinations
-from math import comb
+from functools import lru_cache
 from pprint import pprint
 
 
@@ -48,21 +48,23 @@ class APriori:
         return combos
 
     def get_possible_combinations2(self, k):
-        keys = list(self.itemset)
+        keys = sorted(list(self.itemset))
         if k == 2:
             return combinations(keys, 2)
         combos = set()
         for item in combinations(keys, k - 1):
+            # TODO check necessary sort
             combo = sorted(set(flatten(item)))
             if len(combo) == k:
                 combos.add(tuple(combo))
         return combos
 
     def get_possible_combinations3(self, k):
-        keys = list(self.itemset)
+        keys = sorted(list(self.itemset))
         if k == 2:
             return combinations(keys, 2)
         keys = set(flatten(keys))
+        # TODO check sub-combinations indeed in self.itemset
         return combinations(keys, k)
 
     def initial_single_count(self):
@@ -76,26 +78,46 @@ class APriori:
             if value not in line:
                 return False
         return True
+    
+    # @lru_cache(maxsize=10)
+    # def get_line_combination(self, line, k):
+    #     return combinations(line, k)
 
     def count_support(self, k):
         """Count the number of item combinations for k per line in datafile"""
         next_itemset = defaultdict(lambda: 0)
         count = 0
-        # We need to cast to list as otherwise the generator is consumed on first run through
-        possible_combinations = list(self.get_possible_combinations2(k))
 
+        # ~~We need to cast to list as otherwise the generator is consumed on first run through~~
+        # We do not cast possible_combinations to list here as it is cheaper to create a new generator
+        # for each line. Like 50x cheaper timewise.
+    
         for line in self.get_line():
-            if count != 0 and count % 100 == 0:
+            possible_combinations = self.get_possible_combinations3(k)
+            # ~~TODO> beat 60 seconds for 500~~
+            # TODO> beat 2.5 seconds for 500
+            if count != 0 and count % 1000 == 0:
                 print(f"processed {count} lines")
             # NOTE: Break early for testing purposes
-            if count != 0 and count % 1000 == 0:
-                break
-            for combination in possible_combinations:
-                # TODO This check stands for 95% of the time problem
-                # You can see this by running python -m cProfile filename.py
-                exists = self.check_combination_in_line(line, combination)
-                if exists:
+            # if count != 0 and count % 5000 == 0:
+            #     break
+            # NOTE: This is sorted, I've tested, although could do one assert on one item
+            line_combinations = combinations(line, k)
+            for combination in line_combinations:
+                if combination in possible_combinations:
                     next_itemset[combination] += 1
+
+            # NOTE: tested switching and this takes double the time
+            # for combination in possible_combinations:
+            #     if combination in line_combinations:
+            #         next_itemset[combination] += 1
+
+            # for combination in possible_combinations:
+            # NOTE This check stands for 95% of the time and takes an ungodly amount of time
+            # You can see this by running python -m cProfile filename.py
+            #     exists = self.check_combination_in_line(line, combination)
+            #     if exists:
+            #         next_itemset[combination] += 1
             count += 1
 
         self.itemset = next_itemset
@@ -117,8 +139,10 @@ class APriori:
             self.count_support(current_k)
 
             print(f"k={current_k} - before pruning. Currently {len(self.itemset)} keys")
+            #print(self.itemset)
             self.prune_non_frequent()
             print(f"k={current_k} - after pruning. Currently {len(self.itemset)} keys")
+            #print(self.itemset)
         return self.itemset
 
     def naive_count_support(self, k):
@@ -143,9 +167,9 @@ class APriori:
 def simple_test():
     a_priori = APriori("simple_test.dat", 1)
     candidates = a_priori.get_candidates(3)
-    pprint(candidates)
     assert len(candidates.keys()) == 1, "should only have one set of keys"
     assert candidates[(2, 3, 5)] == 2, "should be 2"
+    print(candidates)
 
 
 def real_test():
@@ -155,5 +179,5 @@ def real_test():
 
 
 if __name__ == "__main__":
-    # simple_test()
+    #simple_test()
     real_test()
