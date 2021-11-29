1ac3c3c1cd96638dc3cd56860f06af8ce44d06b0
Evie <evita@stenqvist.co>
2021-11-29 20:07:33 +0100

    Streaming triangles v3

--- homework_3/streaming_triangles_v2.py
+++ homework_3/streaming_triangles_v2.py
@@ -1,7 +1,9 @@
 import collections
+from dataclasses import dataclass
 import math
 import random
 from itertools import tee
+import gzip
 
 
 def pairwise(iterable):
@@ -9,6 +11,116 @@ def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+@dataclass
+class Edge:
+    left: int
+    right: int
+
+    @staticmethod
+    def from_line(line):
+        return Edge(line[0], line[3])
+
+
+@dataclass
+class Wedge:
+    left: int
+    middle: int
+    right: int
+
+    @staticmethod
+    def from_edges(x, y):
+        if x == y:
+            return False
+        if x.left == y.left:
+            return Wedge(x.right, x.left, y.right)
+        if x.right == y.right:
+            return Wedge(y.left, y.right, x.left)
+        if x.left == y.right:
+            return Wedge(y.left, x.left, x.right)
+        if y.left == x.right:
+            return Wedge(x.left, x.right, y.right)
+        return False
+
+    def closed_by(self, z):
+        if self.left == z.left and self.right == z.right:
+            return True
+        return False
+
+
+assert Wedge.from_edges(Edge(3, 4), Edge(3, 5)).closed_by(
+    Edge(4, 5)
+), "(3,4) (3,5) should be closed by (4,5)"
+assert Wedge.from_edges(Edge(2, 3), Edge(1, 2)).closed_by(
+    Edge(1, 3)
+), "(2,3) (1,2) should be closed by (1,3)"
+assert Wedge.from_edges(Edge(1, 2), Edge(2, 3)).closed_by(
+    Edge(1, 3)
+), "(1,2) (2,3) should be closed by (1,3)"
+assert Wedge.from_edges(Edge(3, 4), Edge(2, 4)).closed_by(
+    Edge(2, 3)
+), "(3,4) (2,4) should be closed by (2,3)"
+
+
+def get_wedge(edge1, edge2):
+    """Check if two edges forms a wedge, and if so returns the wedge.
+
+    :param edge1    : An edge on the form [u,v].
+    :param edge2    : An edge on the form [u,v].
+    :return         : (True, wedge) if the edges form a wedge, otherwise (False, None).
+                        wedge is given on the form (a,b,c), where a < c
+    """
+
+    if edge1 == edge2:
+        return False, None
+
+    for i in range(2):
+        for j in range(2):
+            if edge1[i] == edge2[j]:
+                t = sorted((edge1[1 - i], edge2[1 - j]))
+                return True, (t[0], edge1[i], t[1])
+
+    return False, None
+
+
+def get_tot_wedges(edge_res):
+    """Get the total number of wedges formed by the edges in edge_res.
+
+    :param edge_res: A list of edges.
+    :return: The total numbers of wedges in edge_res.
+    """
+    tot_wedges = set()
+
+    for i in range(len(edge_res)):
+        if edge_res[i] is None:
+            continue
+        for j in range(i):
+            if edge_res[j] is None:
+                continue
+            is_wedge, wedge = get_wedge(edge_res[i], edge_res[j])
+            if is_wedge:
+                tot_wedges.add(wedge)
+
+    return len(tot_wedges)
+
+
+def get_new_wedges(edge_t, edge_res):
+    """Get the new wedges formed by edge_t and all edges in edge_res.
+
+    :param edge_t:
+    :param edge_res:
+    :return: A list of wedges
+    """
+    new_wedges = []
+
+    for edge in edge_res:
+        if edge is not None:
+            is_wedge, wedge = get_wedge(edge, edge_t)
+            if is_wedge:
+                new_wedges.append(wedge)
+
+    return new_wedges
 
 
 class StreamingTriangles:
@@ -42,54 +154,45 @@ class StreamingTriangles:
         self.wedge_res = [None] * s_w
         self.is_closed = [False] * s_w
 
-        # Assuming sampling up to s_e is a part of initialisation...
-        for i in range(s_e):
-            line = open_file.readline()
-            edge = self.edge_from_line(line)
-            self.edge_res[i] = edge
-        print("Initial edge_res", self.edge_res)
-
-        # NOTE: testing by resetting after initing edge_res...
-        # which doesn't seem to work either as all edge_res
-        # are the same in update
-
-        # Init the number of lines we've read
-        self.t = i + 1  # 0
-        # We set the pointer to the top of the file
-        # open_file.seek(0)
-        # NOTE: From these edges, we generate a random wedge by doing a second level of reservoir sampling.
-        # This process implicitly treats the wedges created in the edge reservoir as a stream,
-        # and performs reservoir sampling on that. Overall, this method approxi- mates uniform
-        # random wedge sampling... <- Not really what we're doing, but we are creating wedges.
-        self.wedge_res = self.sample_wedges()
-        print("Initial wedge_res", self.wedge_res, "\n")
+        # Init the current number of total wedges
+        self.tot_wedges = 0
+        # Init the current t
+        self.t = 1
 
         # For each edge e_t in stream call update
+        # for i in range(1000):
         while True:
             line = open_file.readline()
-            if not line:
+            if not line or line == b"\n":
                 break
+            if line.startswith(b"#"):
+                continue
             edge = self.edge_from_line(line)
-            self.t += 1
+            # if not edge:
+            #     continue
             self.update(edge)
-            # Let p be the fraction of entries in isClosed set to true
-            if sum(self.is_closed):
-                import ipdb; ipdb.set_trace()
+            self.t += 1
+            # if self.t % 10000 == 0:
+            #     self.__print_stats()
+        self.__print_stats()
 
-            p = sum(self.is_closed) / len(self.is_closed)
-            print(self.wedge_res)
-            print(self.is_closed)
-            # set k_t = 3p
-            k_t = 3 * p
-            # k_t is the transitivity at t
-            print(f"k_{self.t}: {k_t}")
-            # set T_t = [pt^2/s_e(s_e-1)] x tot_wedges
-            T_t = p ** 2 / (self.s_e * (self.s_e - 1)) * self.tot_wedges
-            # T_t is the number of triangles at t
-            print(f"T_{self.t}: {T_t} \n")
+    def __print_stats(self):
+        # Let p be the fraction of entries in isClosed set to true
+        # import ipdb; ipdb.set_trace()
+        p = sum(self.is_closed) / len(self.is_closed)
+        print(p)
+        # set k_t = 3p
+        k_t = 3 * p
+        # k_t is the transitivity at t
+        print(f"k_{self.t}: {k_t}")
+        # set T_t = [pt^2/s_e(s_e-1)] x tot_wedges
+        T_t = ((p * self.t ** 2) / (self.s_e * (self.s_e - 1))) * self.tot_wedges
+        tau_t = ((p * self.t ** 2) / (self.s_e * (self.s_e - 1))) * self.tot_wedges
+        # T_t is the number of triangles at t
+        print(f"T_{self.t}: {T_t} \n")
 
-    def sample_wedges(self):
-        """Sample wedges from our current self.edge_res
+    def get_wedges(self):
+        """Get all wedges from our current self.edge_res
 
         First create an edge_mapping, i.e. for each vertice x in
         (x, y) set a dictionary key to x and the value to the list
@@ -99,7 +202,7 @@ class StreamingTriangles:
         (1, 2), (1, 3), (2, 4)
 
         Edge mapping:
-        {1: [2, 3], 2: [4]}
+        {1: [2, 3], 2: [4], 3: [1], 4: [2]}
 
         Create a candidate list of possible keys from our edge mapping
         filtering on values that are atleast 2.
@@ -111,7 +214,7 @@ class StreamingTriangles:
         candidates = list(
             filter(lambda vertice: len(edge_mapping[vertice]) >= 2, list(edge_mapping))
         )
-        samples = []
+        wedges = []
         for vertice in candidates:
             other_vertices = edge_mapping[vertice]
             # Insert our vertice inbetween the other two to create a wedge
@@ -119,88 +222,102 @@ class StreamingTriangles:
                 # Although, if we connect to more than 2 other vertices
                 # create all the paths exhaustively
                 for pair in pairwise(other_vertices):
-                    pair = list(pair)
+                    pair = sorted(list(pair))
                     pair.insert(1, vertice)
-                    samples.append(tuple(pair))
+                    wedges.append(tuple(pair))
             else:
-                other_vertices = list(other_vertices)
+                other_vertices = sorted(list(other_vertices))
                 other_vertices.insert(1, vertice)
-                samples.append(tuple(other_vertices))
-        self.tot_wedges = len(samples)
-        assert self.tot_wedges != 0, print(
-            "Total wedges shouldn't be zero", edge_mapping
-        )
-
-        # Sampling should suffice with this random int method seeing as it picks
-        # an int uniformally...
-        if self.tot_wedges > self.s_w:
-
-            truncated_samples = []
-            indices = set()
-            while len(indices) < self.s_w:
-                index = random.randint(0, self.s_w)
-                indices.add(index)
-            for index in indices:
-                truncated_samples.append(samples[index])
-            assert len(truncated_samples) == self.s_w, "Should pick s_w of wedges"
-            # import ipdb; ipdb.set_trace()
-            return truncated_samples
-        # import ipdb; ipdb.set_trace()
-        return samples
+                wedges.append(tuple(other_vertices))
+        #self.tot_wedges = len(wedges)
+        return wedges
 
     @staticmethod
     def edge_from_line(line):
         """Get an edge in the form of a tuple from a line
         Assumes verticies are \t delimited
         """
-        return (line[0], line[3])
+        decoded = sorted(line.decode().strip().split("\t"))
+        edge = (int(decoded[0]), int(decoded[1]))
+        return edge
 
     def update(self, e_t):
-        print("Current e_t is, ", e_t)
-        print("Current edge_res is, ", self.edge_res)
         # Steps 1-3 determines all the wedges in the wedge reservoir that are closed by e_t
         # and updates is_closed accordingly.
+        for i in range(self.s_w):
+            if self.wedge_res[i] and e_t == (
+                self.wedge_res[i][0],
+                self.wedge_res[i][2],
+            ):
+                self.is_closed[i] = True
+
+        # if self.t % 100000 == 0:
+        #     print(self.wedge_res)
+
         # Steps 4-7 we perform reservoir sampling on edge_res. This involved replacing each entry by
         # e_t with probability 1/t, remaining steps are executed iff this leads to any changes in edge_res
+        edge_res_was_updated = False
+
+        for i in range(self.s_e):
+            x = random.random()
+            if x <= 1 / self.t:
+                edge_res_was_updated = True
+                self.edge_res[i] = e_t
+                # print("Updated edge_res!", self.edge_res)
+
         # We perform some updates to tot wedges and determine the new wedges Nt (new_wedges_t).
         # Finally, in Steps 11-16, we perform reservoir sampling on wedge res, where each entry is
         # randomly replaced with some wedge in Nt. Note that we may remove wedges that have already closed.
-        for i in range(self.s_w):
-            # if wedge_res[i] is closed by e_t, Steps 1-3.
-            wedge = self.wedge_res[i]  # wedge: [start, middle, end]
-            if e_t == (wedge[0], wedge[2]):
-                self.is_closed[i] = True
-
-        edge_res_was_updated = False
-        # Perform reservoir sampling, steps 4-7
-        for i in range(self.s_e):
-            if e_t not in self.edge_res:
-                # import ipdb; ipdb.set_trace()
-                x = random.random()
-                if x <= 1 / self.t:
-                    edge_res_was_updated = True
-                    # TODO: I don't get this, could become
-                    # self.edge_res = [same, same, same...] if <= 1/self.t is
-                    # high as in the beginning if we reset file and t = 0
-                    self.edge_res[i] = e_t
-                    print("Updated edge_res!", self.edge_res)
-        # Remaining steps
         if edge_res_was_updated:
-            self.wedge_res
-            samples = self.sample_wedges()
-            new_wedges = list(
-                filter(lambda wedge: wedge not in self.wedge_res, samples)
-            )
-            len_new_wedges = len(new_wedges)
+            # self.wedge_res
+            all_wedges = get_tot_wedges(self.edge_res)
+            #all_wedges = self.get_wedges()
+            new_wedges = get_new_wedges(e_t, self.edge_res)
+            # new_wedges = list(
+            #     filter(lambda wedge: wedge not in self.wedge_res, all_wedges)
+            # )
+            #new_wedges = get_new_wedges(e_t, self.edge_res)
+            self.tot_wedges = all_wedges
+
+            # samples = self.sample_wedges()
+            # new_wedges = list(
+            #     filter(lambda wedge: wedge not in self.wedge_res, samples)
+            # )
+            # len_new_wedges = len(new_wedges)
             # If no new wedges?
-            if not len_new_wedges:
-                return
             for i in range(self.s_w):
+                if not len(new_wedges):
+                    return
                 x = random.random()
-                if x <= len_new_wedges / self.tot_wedges:
-                    w_index = random.randint(0, len_new_wedges - 1)
+                if x <= len(new_wedges) / self.tot_wedges:
+                    w_index = random.randint(0, len(new_wedges) - 1)
                     self.wedge_res[i] = new_wedges[w_index]
                     self.is_closed[i] = False
 
 
-StreamingTriangles(3, 2, open("toy_graph.txt", "r"))
+# StreamingTriangles(4, 3, open("toy_graph.txt", "r"))
+# for i in range(10):
+#     StreamingTriangles(200, 200, gzip.open("ca-AstroPh.txt.gz", "rb"))
+
+StreamingTriangles(450, 450, gzip.open("ca-AstroPh.txt.gz", "rb"))
+
+
+# for i in [100, 200, 300, 400]:
+#     StreamingTriangles(i, i, gzip.open("ca-AstroPh.txt.gz", "rb"))
+
+# for i in [500, 600, 700]:
+#     StreamingTriangles(i, i, gzip.open("ca-AstroPh.txt.gz", "rb"))
+
+
+# CondMat
+# Average clustering coefficient	0.6334
+# Number of triangles	173361
+
+# Astro
+# Average clustering coefficient	0.6306
+# Number of triangles	1 351 441
+
+# 1 380 156.7405113066
+# 4 251 647.682809698
+
+# 5 062 318
---
+++ homework_3/streaming_triangles_v3.py
@@ -0,0 +1,237 @@
+import collections
+from dataclasses import dataclass
+import random
+from itertools import tee, chain
+import gzip
+
+
+def pairwise(iterable):
+    """pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    Borrowed directly from python itertools (pairwise was introcued in 3.10
+    and we don't want to update python)"""
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def flatten(list_of_lists):
+    """Flatten one level of nesting
+    Borrowed directly from python itertools recipes
+    """
+    return chain.from_iterable(list_of_lists)
+
+
+@dataclass
+class Edge:
+    left: int
+    right: int
+
+    @staticmethod
+    def from_line(line):
+        decoded = sorted(line.decode().strip().split("\t"))
+        edge = (int(decoded[0]), int(decoded[1]))
+        return Edge(edge[0], edge[1])
+
+    def as_tuple(self):
+        return (self.left, self.right)
+
+
+@dataclass
+class Wedge:
+    left: int
+    middle: int
+    right: int
+
+    @staticmethod
+    def from_edges(x, y):
+        if len(set(flatten([x.as_tuple(), y.as_tuple()]))) != 3:
+            # Short circuit by checking if the set of our edges isn't 3,
+            # i.e. the set is 2, x == y, or 4 -> they have nothing in common
+            return False
+        if x.left == y.left:
+            return Wedge(x.right, x.left, y.right)
+        if x.right == y.right:
+            return Wedge(y.left, y.right, x.left)
+        if x.left == y.right:
+            return Wedge(y.left, x.left, x.right)
+        if y.left == x.right:
+            return Wedge(x.left, x.right, y.right)
+        return False
+
+    def closed_by(self, z):
+        if self.left == z.left and self.right == z.right:
+            return True
+        # NOTE: shouldn't need to check other way as both are sorted
+        # if self.left == z.right and self.right == z.left:
+        #     return True
+        return False
+
+    def __hash__(self):
+        return hash((self.left, self.middle, self.right))
+
+
+def get_wedges(edge_res):
+    """Get all wedges from our current self.edge_res
+
+    First create an edge_mapping, i.e. for each vertice x in
+    (x, y) set a dictionary key to x and the value to the list
+    of vertices the vertice x has an edge with.
+
+    Example:
+    (1, 2), (1, 3), (2, 4)
+
+    Edge mapping:
+    {1: [2, 3], 2: [4], 3: [1], 4: [2]}
+
+    Create a candidate list of possible keys from our edge mapping
+    filtering on values that are atleast 2.
+
+    Then create all possible wedges given these candidates.
+    """
+    edge_mapping = collections.defaultdict(set)
+    for edge in edge_res:
+        edge_mapping[edge.left].add(edge.right)
+        edge_mapping[edge.right].add(edge.left)
+    candidates = list(
+        filter(lambda vertice: len(edge_mapping[vertice]) >= 2, list(edge_mapping))
+    )
+    wedges = set()
+    for vertice in candidates:
+        other_vertices = sorted(edge_mapping[vertice])
+        # Insert our vertice inbetween the other two to create a wedge
+        if len(other_vertices) > 2:
+            # Although, if we connect to more than 2 other vertices
+            # create all the paths exhaustively
+            for pair in pairwise(other_vertices):
+                pair = list(pair)
+                pair.insert(1, vertice)
+                wedges.add(Wedge(*pair))
+        else:
+            other_vertices = sorted(list(other_vertices))
+            other_vertices.insert(1, vertice)
+            wedges.add(Wedge(*other_vertices))
+    return len(wedges)
+
+
+def get_new_wedges(edge_t, edge_res):
+    """Get the new wedges formed by edge_t and all edges in edge_res."""
+    new_wedges = []
+
+    for edge in edge_res:
+        wedge = Wedge.from_edges(edge, edge_t)
+        if wedge:
+            new_wedges.append(wedge)
+
+    return new_wedges
+
+
+def __print_stats(is_closed: list[bool], s_e: int, t: int, tot_wedges: int):
+    # Let p be the fraction of entries in isClosed set to true
+    p = sum(is_closed) / len(is_closed)
+    # set k_t = 3p
+    k_t = 3 * p
+    # k_t is the transitivity at t
+    print(f"k_{t}: {k_t}")
+    # set T_t = [pt^2/s_e(s_e-1)] x tot_wedges
+    T_t = ((p * (t ** 2)) / (s_e * (s_e - 1))) * tot_wedges
+    # T_t is the number of triangles at t
+    print(f"T_{t}: {T_t} \n")
+
+
+def main(open_file, s_e: int, s_w: int):
+    """The main part of the algorithm which calls update on every edge from the stream"""
+
+    # Initialise our data structures
+    edge_res = [None] * s_e
+    wedge_res = [None] * s_w
+    is_closed = [False] * s_w
+    tot_wedges = 0
+
+    # Iterate over every vertice in our file
+    t = 1
+    while True:
+        line = open_file.readline()
+        if not line or line == b"\n":
+            break
+        if line.startswith(b"#"):
+            # Don't bother with comments
+            continue
+        edge = Edge.from_line(line)
+        tot_wedges = update(
+            edge, t, s_e, s_w, edge_res, wedge_res, tot_wedges, is_closed
+        )
+        t += 1
+
+        if t % 1000 == 0:
+            __print_stats(is_closed, s_e, t, tot_wedges)
+
+    __print_stats(is_closed, s_e, t, tot_wedges)
+
+
+def update(
+    edge: Edge,
+    t: int,
+    s_e: int,
+    s_w: int,
+    edge_res: list[Edge],
+    wedge_res: list[Wedge],
+    tot_wedges: int,
+    is_closed: list[bool],
+):
+    """The update stage of the algorithm, the "steps" comments are directly taken from the paper"""
+
+    # Steps 1-3 determines all the wedges in the wedge reservoir that are closed by e_t
+    # and updates is_closed accordingly.
+    for i in range(s_w):
+        if wedge_res[i] and wedge_res[i].closed_by(edge):
+            is_closed[i] = True
+
+    # Steps 4-7 we perform reservoir sampling on edge_res. This involved replacing each entry by
+    # e_t with probability 1/t, remaining steps are executed iff this leads to any changes in edge_res
+    updated = False
+    for i in range(s_e):
+        x = random.random()
+        if x <= 1 / t:
+            updated = True
+            edge_res[i] = edge
+
+    # We perform some updates to tot wedges and determine the new wedges Nt (new_wedges_t).
+    # Finally, in Steps 11-16, we perform reservoir sampling on wedge res, where each entry is
+    # randomly replaced with some wedge in Nt. Note that we may remove wedges that have already closed.
+    if updated:
+        tot_wedges = get_wedges(edge_res)
+        new_wedges = get_new_wedges(edge, edge_res)
+        if not new_wedges:
+            return tot_wedges
+        for i in range(s_w):
+            x = random.random()
+            if x <= len(new_wedges) / tot_wedges:
+                index = random.randint(0, len(new_wedges) - 1)
+                wedge_res[i] = new_wedges[index]
+                is_closed[i] = False
+    return tot_wedges
+
+
+def tests():
+    assert Wedge.from_edges(Edge(3, 4), Edge(3, 5)).closed_by(
+        Edge(4, 5)
+    ), "(3,4) (3,5) should be closed by (4,5)"
+    assert Wedge.from_edges(Edge(2, 3), Edge(1, 2)).closed_by(
+        Edge(1, 3)
+    ), "(2,3) (1,2) should be closed by (1,3)"
+    assert Wedge.from_edges(Edge(1, 2), Edge(2, 3)).closed_by(
+        Edge(1, 3)
+    ), "(1,2) (2,3) should be closed by (1,3)"
+    assert Wedge.from_edges(Edge(3, 4), Edge(2, 4)).closed_by(
+        Edge(2, 3)
+    ), "(3,4) (2,4) should be closed by (2,3)"
+
+
+if __name__ == "__main__":
+    tests()
+    main(gzip.open("web-NotreDame.txt.gz", "rb"), 20000, 20000)
+
+## web-NotreDame
+# Average clustering coefficient	0.2346
+# Number of triangles	8910005
+# Fraction of closed triangles	0.03104
