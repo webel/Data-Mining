d60511b866be49651b28cd2da25ba5e54938aecb
Evie <evita@stenqvist.co>
2021-12-05 16:16:26 +0100

    🤓  Algorithm done!

--- homework_4/main.py
+++ homework_4/main.py
@@ -1,79 +1,107 @@
-import csv
-import math
-from operator import inv
-
-# As a warm up task, you should first compute the eigenvectors and
-# eigenvalues of a set of graphs, and find out how many communities
-# do these graphs have.
-
 """
 Implementation of K-eigenvector Algorithm
+
+Citation: 
+    Ng, A. Y., Jordan, M. I., & Weiss, Y. (2002). 
+    On spectral clustering: Analysis and an algorithm. 
+    In Advances in neural information processing systems (pp. 849-856).
 """
-sigma = 0.4
+import numpy as np
 
 
-def column(matrix, i):
-    return [row[i] for row in matrix]
-
-
-def load_matrix_from_csv_file(file_path: str):
-    matrix = []
-    with open(file_path, "r") as f:
-        for line in csv.reader(f):
-            matrix.append(list(map(int, line)))
-        # yield from csv.reader(f)
-    return matrix
-
-
-def gaussian_kernel(p1, p2):
-    return math.exp(-(math.dist(p1, p2) ** 2 / (2 * sigma ** 2)))
-
-
-def form_affinity_matrix(edge_list: list):
-    """Convert edge list to affinity matrix
-    Step 1.
+def kMeans(X, K, maxIters=10):
+    """kMeans clustering algorithm
+    Tweaked from https://gist.github.com/bistaumanga/6023692
+    as to not need unnecessary scipy import.
     """
-    # col1 = column(edge_list, 1)
-    # col2 = column(edge_list, 2)
-    #
-    n = len(edge_list[0])
-    A = []
-    for i in range(n):
-        A.append([0] * n)
-        for j in range(n):
-            if i == j:
-                continue
-            A[i][j] = gaussian_kernel(edge_list[i], edge_list[j])
+    centroids = X[np.random.choice(np.arange(len(X)), K), :]
+    for i in range(maxIters):
+        # Cluster Assignment step
+        C = np.array(
+            [
+                np.argmin([np.dot(x_i - y_k, x_i - y_k) for y_k in centroids])
+                for x_i in X
+            ]
+        )
+        # Move centroids step
+        centroids = [X[C == k].mean(axis=0) for k in range(K)]
+    return np.array(centroids), C
+
+
+def load_edgelist_from_csv_file(file_path: str):
+    return np.loadtxt(file_path, delimiter=",", dtype=int)
+
+
+def form_affinity_matrix(S):
+    """Provided an edge_list S return the affinity matrix for the nodes"""
+    # Using the fact that ||x-y||^2 = ||x||^2 + ||y||^2 - 2 * x^T * y.
+    # S_norm = np.sum(S ** 2, axis=-1)
+    # return np.exp(
+    #     -(S_norm[:, None] + S_norm[None, :] - 2 * np.dot(S, S.T)) / 2 * sigma ** 2
+    # )
+    # NOTE: the above works well to cluster edges, but we want
+    # to cluster the points given that there exists an edge, which is binary.
+    n = np.unique(S).size  # Gives us the number of nodes
+    A = np.zeros((n, n))
+    for edge in S:
+        A[edge[0] - 1][edge[1] - 1] = 1
+        A[edge[1] - 1][edge[0] - 1] = 1
     return A
 
-    # for j in range(n)
-    # for v, u in load_matrix_from_csv_file('./example1.dat'):
 
-def elementwise(matrix, func):
-    ''''''
-    n_rows = len(matrix)
-    result = []
-    for i in range(n_rows):
-        row = list(map(func, matrix[i]))
-        result.append(row)
-    return result
+def construct_l(A):
+    D = np.diag(np.sum(A, axis=1))
+    D_inv = np.linalg.inv(np.sqrt(D))
+    # Return the constructed L matrix
+    return np.dot(np.dot(D_inv, A), D_inv)
 
 
-def construct_l(adjacency_matrix: list[list]):
-    n = len(adjacency_matrix[0])
-    D = [[0]*n for _ in range(n)]
-    L = [[0]*n for _ in range(n)]
-    for i in range(n):
-        D[i][i] = sum(adjacency_matrix[i])
-        inv_D = D[i][i]**(-1/2) 
-        L[i][i] = inv_D * adjacency_matrix[i][i] * inv_D
-    return L
-    #D_i = elementwise(D, lambda x: x**(-1/2))
+def k_largest_eigenvectors(L, k):
+    """Get the k largest eigenvectors from L"""
+    # Returns eigenvectors as columns with largest to the right
+    eigenvalues, eigenvectors = np.linalg.eigh(L)
+    # return k largest eigenvalues & eigenvectors
+    # by slicing the matrix at the kth column from
+    # the right
+    return eigenvalues[-k:], eigenvectors[:, -k:]
 
-def k_largest_eigenvectors(L):
-    pass
+
+def renormalize_x(X):
+    """Step 4: Form matrix Y by renormalizing X"""
+    return X / np.sqrt(np.sum(X ** 2, axis=1)).reshape(-1, 1)
+
+
+def plot_graph(edge_list, labels):
+    import networkx as nx
+    import matplotlib.pyplot as plt
+
+    graph = nx.Graph()
+    graph.add_edges_from(edge_list)
+    plt.figure()
+    nx.draw(graph, node_color=labels, with_labels=True, font_color="white")
+    plt.show()
+
+
+def nips_clustering_algorithm(data_path, k, plot=False):
+    # Base step, load the edges S.
+    edge_list = load_edgelist_from_csv_file(data_path)
+    # Step 1: Form the affinity matrix
+    adjacency_matrix = form_affinity_matrix(edge_list)
+    # Step 2: Construct L
+    L = construct_l(adjacency_matrix)
+    # Step 3: Get the k largest eigenvectors of L
+    # NOTE: potentially use eigenvalues to choose k, note
+    # sure yet if the penny has dropped on how
+    _, X = k_largest_eigenvectors(L, k)
+    # Step 4: Form matrix Y from X by renormalizing X to unit length
+    Y = renormalize_x(X)
+    # Step 5: Cluster given arbitrary algorithm, here using kMeans
+    # as suggested in paper
+    _, labels = kMeans(Y, k)
+    print("Label for each node", labels)
+    if plot:
+        plot_graph(edge_list, labels)
+
 
 if __name__ == "__main__":
-    matrix = load_matrix_from_csv_file("./example1.dat")
-    adjacency_matrix = form_affinity_matrix(matrix)
-    L = construct_l(adjacency_matrix)
+    nips_clustering_algorithm("simple.dat", 2)
